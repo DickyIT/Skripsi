bd9a5c9cd106293d2e84d1c9f3d98da9d70c4cf2
DickyIT <mohdickypurnama@gmail.com>
2026-05-09 14:29:30 +0700

    fix railway backend start

--- backend/server.js
+++ backend/server.js
@@ -429,7 +429,7 @@ app.delete("/users/:id", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`🚀 Backend running on ${PORT}`);
